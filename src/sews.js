adb5b2b9a25d6cf5e78ba8d1409fdd3baa5d1a1a
Iwan van der Kleijn <iwanvanderkleijn@gmail.com>
2015-05-24 18:17:14 +0200

    Version 0.0.1 - README written

--- src/sews.js
+++ src/sews.js
@@ -1,17 +1,33 @@
+// #### Sews.js - Simple Eventbus for Web Sockets
+// ##### v 0.0.1 - Documentation generated with the lovely [Docco](http://jashkenas.github.com/docco/)
+// 
 // > Copyright (c) 2015 Iwan van der Kleijn
 // > All rights reserved.
 //
-// > This source code is licensed under the BSD-style license found in the LICENSE file in the root directory of this source tree
+// > This source code is licensed under the BSD-style license found in the LICENSE file in the root directory of this source tree.
 // 
-// This file is part of [Sews](sews.html)
+// Be aware that Sews is written in ECMAScript 6 (2015). If to be executed in an ECMAScript 5 compatible run-time, you´ll need
+// [Babel](http://babeljs.io) to compile, or transpile, the source files (in src) to their transformed target files (in dist).
+// For an excellent overview of ES6 see: [https://babeljs.io/docs/learn-es6/](https://babeljs.io/docs/learn-es6/)
+
+// ##### Begin Sews.js source
 
 // Import Web Socket library
 import * as ws from 'ws';
 import WebSocket from 'ws';
+// 
 import {EventEmitter} from 'events';
 
+//
+// 
 // bus.error bus.open bus.message bus.unknown
 
+// *
+// The Eventbus consist of a server and a client. The server is the central node implementing the "Bus". From the client´s view, 
+// there is no difference between a "server" and a "bus" as *any* form of communication is in the form of sending and receiving 
+// events to and from the "bus". There is no concept of "request/response" or "RPC" (remote procedure call).
+//
+//
 class WsBus extends EventEmitter {
   
   constructor(options){
@@ -22,16 +38,20 @@ class WsBus extends EventEmitter {
       let wc = new WsClient(socket);
       this.emit('connection', wc);
       socket.on('message', (message) => {
+        console.log('on server: message received', message);
         this.emit('bus.message', message);
         
         try{
           let msg = JSON.parse(message);
           if(!msg.topic || !this._events[msg.topic]){
+            console.log('on server: unknown message', msg);
             this.emit('bus.unknown', msg);
           } else {
+            console.log('on server: message passed on', msg); 
             this.emit(msg.topic, msg.data, wc);
           }
         }catch(err){
+          console.log('on server: message error', err);
           this.emit('bus.error', err);
         }
       });
@@ -67,15 +87,18 @@ class WsClient extends EventEmitter {
       this.emit('bus.open');
     });
     this.ws.on('message', (message) => {
-      
+      console.log('message received', message);
       try{
         let msg = JSON.parse(message);
         if(!msg.topic || !this._events[msg.topic]){
           this.emit('bus.unknown', msg);
+          console.log('message unknown', msg);
         } else {
+          console.log('message passed on', msg);
           this.emit(msg.topic, msg.data, this);
         }
       }catch(err){
+        console.log('message error:', error);
         this.emit('bus.error', err);
       }
     });
@@ -101,34 +124,3 @@ export default {
     return new WsClient(url, callback);
   }
 };
-
-/*
-Event: 'error'
-
-function (error) { }
-
-If the underlying server emits an error, it will be forwarded here.
-
-Event: 'headers'
-
-function (headers) { }
-
-Emitted with the object of HTTP headers that are going to be written to the Stream as part of the handshake.
-
-Event: 'connection'
-
-function (socket) { }
-
-
-
-let wss = new ws.Server({port:9000});
-wss.on('connection', (ws) => {
-  ws.on('message', (message)=> {
-    console.log('received: %s', message);
-    ws.send('received:' + JSON.stringify(message));
-  });
-
-  ws.send('connected');
-});
-
-*/
